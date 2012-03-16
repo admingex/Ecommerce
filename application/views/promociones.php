e9ccc58d4a8c1cd316cf92614bbd7bbd6f0a8c82
unknown <gestrada956@Gee-gestrada956.corp.ad.timeinc.com>
2012-03-16 09:38:39 -0600

    Obtener detalle de los productos

--- application/views/promociones.php
+++ application/views/promociones.php
@@ -6,15 +6,38 @@
 		include('promociones/listar.html');
 	}
 	if($detalle){
-		if(!empty($id_sitio)){
-			echo "<br />sitio: ".$id_sitio;
+		if(!empty($sitio)){
+			echo "<br />sitio: <a href='".$sitio->urlVc."' target='new'>".$sitio->urlVc."</a>";
+			echo json_encode($sitio);	
+		}		
+			
+		if(!empty($canal)){
+			echo "<br/>
+				  <br />
+				  Canal:
+				  <br />descripcion: ".$canal->descripcionVc."
+				  <br />addkey: ".$canal->addKeyVc;
+				  echo json_encode($canal);				
+		}		
+		
+		if(!empty($promocion)){
+			echo "<br />
+			      <br />
+			      Promocion
+			      <br />descripcion: ".$promocion->descripcionVc."
+			      <br />duracion: ".$promocion->inicio_promocionDt."-".$promocion->fin_promocionDt;
+				  echo json_encode($promocion);	
+			if(!empty($articulos)){
+				echo "<br />
+			          <br />
+			      	  Articulos";		
+			    foreach($articulos as $articulo){
+			    	echo "<br /><br />tipo producto: ".$articulo['tipo_productoVc']."
+			          <br />medio entrega ".$articulo['medio_entregaVc'];
+			    }  	
+				echo "<span style='background-color: #FFF'>".json_encode($articulos)."</span>";	  	      	  			    			         
+			}		  
 		}
-		if(!empty($id_promocion)){
-			echo "<br />promocion: ".$id_promocion;
-		}	
-		if(!empty($id_canal)){
-			echo "<br />canal: ".$id_canal;
-		}
-	}
+	}			
 	?>		
 </div>
