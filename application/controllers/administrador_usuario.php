8d26ca42af652bdb2acfd80b83191b73a2d1cca0
Giovanni Estrada Alemán <vagio12345@gmail.com>
2012-09-12 21:03:27 -0500

    Administrador de usuario

--- application/controllers/administrador_usuario.php
+++ application/controllers/administrador_usuario.php
@@ -10,6 +10,8 @@ class Administrador_usuario extends CI_Controller {
 		
 		// incluye el modelo de las direcciones de facturacion		
 		$this->load->model('direccion_facturacion_model', 'direccion_facturacion_model', true);				
+		$this->load->model('direccion_envio_model', 'direccion_envio_model', true);
+		$this->load->model('forma_pago_model', 'forma_pago_model', true);
     }
 	
 	public function index()
@@ -17,7 +19,17 @@ class Administrador_usuario extends CI_Controller {
 	}
 	
 	public function listar_razon_social($id_cliente = ""){		
-		$data['razon_social'] = $this->direccion_facturacion_model->listar_razon_social($id_cliente);					
+		$data['rs'] = $this->direccion_facturacion_model->listar_razon_social($id_cliente);						
+		echo json_encode($data);
+	}
+	
+	public function listar_direccion_envio($id_cliente = ""){
+		$data['direccion_envio'] = $this->direccion_envio_model->listar_direcciones($id_cliente)->result_array();							
+		echo json_encode($data);
+	}
+	
+	public function listar_tarjetas($id_cliente = ""){
+		$data['direccion_envio'] = $this->forma_pago_model->listar_tarjetas($id_cliente)->result_array();							
 		echo json_encode($data);
 	}
 	
