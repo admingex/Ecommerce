4d520c1da18aac4dff68bdb73e96c30302046b02
Giovanni Estrada Alemán <vagio12345@gmail.com>
2012-09-19 13:17:19 -0500

    Ajuste detalle de compras

--- application/controllers/administrador_usuario.php
+++ application/controllers/administrador_usuario.php
@@ -2,6 +2,13 @@
 
 include('api.php');
 class Administrador_usuario extends CI_Controller {
+	
+	public static $FORMA_PAGO = array(
+		1 =>	"Prosa", 
+		2 =>	"American Express", 
+		3 =>	"Deposito Bancario",
+		4 =>	"Otro"
+	);
 		
 	function __construct()
     {
@@ -14,6 +21,7 @@ class Administrador_usuario extends CI_Controller {
 		$this->load->model('direccion_envio_model', 'direccion_envio_model', true);
 		$this->load->model('forma_pago_model', 'forma_pago_model', true);
 		$this->load->model('login_registro_model', 'login_registro_model', true);
+		$this->load->model('reporte_model', 'reporte_model', true);
 		
 		$this->api = new Api();
     }
@@ -33,27 +41,227 @@ class Administrador_usuario extends CI_Controller {
 		}				
 	}
 	
-	public function actualizar_cliente($id_cliente = ""){		
-		if($_POST){
+	public function compras_cliente(){		
+		$data['id_cliente'] = $id_cliente = $_POST['id_cliente'];
+		//$data['id_cliente'] = $id_cliente = 28;		 
+		$compras_cliente = $this->reporte_model->obtener_compras_cliente($id_cliente);
+		if($compras_cliente->num_rows()>0){
+			$data['compras'] = array();
+			$todas_compras = $compras_cliente->result_array();			
+			foreach($todas_compras as $ind => $compra){
+				$id_compra = $compra['id_compraIn'];				
+				$data['compras'][$ind]['compra'] = $compra;
+				
+				//se obtiene el medio y la fecha de pago
+				$forma_pago = $this->reporte_model->obtener_medio_pago($id_compra, $id_cliente);
+				if($forma_pago -> num_rows > 0){
+					$data['compras'][$ind]['medio_pago'] = self::$FORMA_PAGO[($forma_pago->row()->id_tipoPagoSi)];	
+					$data['compras'][$ind]['fecha_compra'] = 	$forma_pago->row()->fecha_registroTs;				
+				}
+				else{
+					$data['compras'][$ind]['medio_pago'] = "no existe";
+					$data['compras'][$ind]['fecha_pago'] = "no existe";					
+				}
+				
+				$ca = $this->reporte_model->obtener_codigo_autorizacion($id_compra, $id_cliente);
+				if($ca->num_rows() > 0){
+					$data['compras'][$ind]['respuesta_banco'] = $ca->row()->respuesta_bancoVc;
+				}			
+				else{
+					$data['compras'][$ind]['respuesta_banco'] = "no hay respuesta";
+				}
+				
+				//se obtiene el id de promocion de la compra
+				$id_promo = $this->reporte_model->obtener_promo_compra($id_compra, $id_cliente);
+				
+				// se obtiene el detalle de la promocion
+				$promocion = $this->reporte_model->obtener_detalle_promo($id_promo);	
+				if($promocion->num_rows()>0){
+					$data['compras'][$ind]['promocion'] = $promocion->row();
+				}
+				//se obtiene el total de articulos en la promocion y el total que se pago por ellos 
+				$articulos_res = $this->reporte_model->obtener_articulos($id_promo);
+				$articulos = $articulos_res->result_array();							 
+				$monto = 0;
+
+				// Se obtienen los articulos de cada promocion y el total pagado por ellos 							
+				foreach( $articulos as $i => $articulo){
+					if($articulo['issue_id']){
+						$issue = $this->reporte_model->obtener_issue($articulo['issue_id']);						
+						$articulos[$i]['tipo_productoVc']= $issue->row()->descripcionVc;
+					}
+					else{
+						$articulos[$i]['tipo_productoVc'] = $articulo['tipo_productoVc'];
+					}
+					$monto+= $articulo['tarifaDc'];
+				}
+				$data['compras'][$ind]['articulos'] = $articulos;
+				/*
+				echo "<pre>";
+					print_r($data);
+				echo "</pre>";
+				*/					
+				
+				$data['compras'][$ind]['monto'] = $monto;
+												
+			}
+		}
+		else{
+			$data['compras'] = NULL;
+		}
+		
+		$this->load->view('reportes/reporte_compras_usuario', $data);		
+	}
+
+	public function detalle_compra($id_compra = "", $id_cliente = ""){
+		$data['compra']['id_compra'] = $id_compra; 
+		$data['compra']['direccion_amex'] = NULL;
+		$data['compra']['codigo_autorizacion'] = NULL;
+		
+		//se obtiene el medio y la fecha de pago
+		$forma_pago = $this->reporte_model->obtener_medio_pago($id_compra, $id_cliente);
+		
+		if($forma_pago -> num_rows > 0){
+			//si el pago es con prosa se obtiene el detalle de la tarjeta
+			if(($forma_pago->row()->id_tipoPagoSi == 1) || ($forma_pago->row()->id_tipoPagoSi == 2)){				
+				$tc = $this->reporte_model->obtener_tc($id_cliente, $forma_pago->row()->id_tipoPagoSi);				
+				$data['compra']['medio_pago'] = $tc->row()->descripcionVc." terminación ".$tc->row()->terminacion_tarjetaVc;				
+					
+				//se obtiene el codigo de autorizacion si es que existe
+				$ca = $this->reporte_model->obtener_codigo_autorizacion($id_compra, $id_cliente);
+				if($ca->num_rows() > 0 ){									
+					if($ca->row()->codigo_autorizacionVc > 0){
+						$data['compra']['codigo_autorizacion'] = "<span class='info-negro'>codigo de autorización:</span> ".$ca->row()->codigo_autorizacionVc;
+					}
+					else{
+						$data['compra']['codigo_autorizacion'] = "<span class='info-negro'>codigo de autorización:</span> ".$ca->row()->codigo_autorizacionVc ."<br />". $ca->row()->respuesta_bancoVc ;
+					}
+				}	
+				else{
+					$data['compra']['codigo_autorizacion'] = "<span class='info-negro'>(No se realizo el cobro)</span>";	
+				}
+					
+			}
+			else{				
+				$data['compra']['medio_pago'] = self::$FORMA_PAGO[($forma_pago->row()->id_tipoPagoSi)];					
+			}
 			
+			//si el pago es con amex se obtiene el detalle de la tarjeta y la direccion de amex
+			if($forma_pago->row()->id_tipoPagoSi == 2){				
+				$data['compra']['direccion_amex'] = "direccion ammex";	
+			}
+										
+			$data['compra']['fecha_compra'] = 	$forma_pago->row()->fecha_registroTs;				
+		}
+		else{
+			$data['compra']['medio_pago'] = NULL;
+			$data['compra']['fecha_pago'] = NULL;				
+		}
+				
+		//se obtiene la direccion de envio si es que existe			
+		$dir_envio = $this->reporte_model->obtener_dir_envio($id_compra, $id_cliente);
+		if($dir_envio->num_rows() > 0){
+				$data['compra']['dir_envio'] = 	$dir_envio->row()->address1." ".
+												$dir_envio->row()->address2." ".
+												$dir_envio->row()->address4."<br />".
+												$dir_envio->row()->zip."<br />".
+														$dir_envio->row()->address3."<br />".
+														$dir_envio->row()->city."<br />".
+														$dir_envio->row()->state;	
+		}
+		else{
+			$data['compra']['dir_envio']= "No requiere";
+		}
+		
+		//se obtiene la direccion de facturacion y Razon Social
+		$facturacion = $this->reporte_model->obtener_facturacion($id_compra, $id_cliente);
+		if($facturacion->num_rows() > 0){								
+			$consecutivo = $facturacion->row()->id_consecutivoSi;
+			$id_rs = $facturacion->row()->id_razonSocialIn;
+			
+			$dir_facturacion = $this->reporte_model->obtener_dir_facturacion($consecutivo, $id_cliente);				
+			$data['compra']['dir_facturacion']  =  $dir_facturacion->row()->address1." ".
+														$dir_facturacion->row()->address2." ".
+														$dir_facturacion->row()->address4."<br />".
+														$dir_facturacion->row()->zip."<br />".
+														$dir_facturacion->row()->address3."<br />".
+														$dir_facturacion->row()->city."<br />".
+														$dir_facturacion->row()->state;
+			
+			$rs = $this->reporte_model->obtener_razon_social($id_rs);
+			$data['compra']['razon_social'] = $rs->row()->company."<br />".$rs->row()->tax_id_number;											
+			
+		}						
+		else{
+			$data['compra']['dir_facturacion'] = NULL;
+			$data['compra']['razon_social'] = NULL;
+		}
+		
+		//se obtiene el id de promocion de la compra
+		$id_promo = $this->reporte_model->obtener_promo_compra($id_compra, $id_cliente);
+		
+		// se obtiene el detalle de la promocion
+		$promocion = $this->reporte_model->obtener_detalle_promo($id_promo);	
+		if($promocion->num_rows()>0){
+			$data['compra']['promocion'] = $promocion->row();
+		}
+					
+		
+		//se obtiene el total de articulos en la promocion y el total que se pago por ellos 
+		$articulos_res = $this->reporte_model->obtener_articulos($id_promo);
+		$articulos = $articulos_res->result_array();							 
+		$monto = 0;
+
+		// Se obtienen los articulos de cada promocion y el total pagado por ellos 							
+		foreach( $articulos as $i => $articulo){
+			if($articulo['issue_id']){
+				$issue = $this->reporte_model->obtener_issue($articulo['issue_id']);						
+				$articulos[$i]['tipo_productoVc']= $issue->row()->descripcionVc;
+			}
+			else{
+				$articulos[$i]['tipo_productoVc'] = $articulo['tipo_productoVc'];
+			}
+			$monto+= $articulo['tarifaDc'];
+		}
+		$data['compra']['articulos'] = $articulos;
+		$data['compra']['monto'] = $monto;
+		
+		// Obtiene informacion de tarjeta
+		/*				
+		echo "<pre>";
+			print_r($data);
+		echo "</pre>";
+		*/											
+		$this->load->view('reportes/detalle_compra', $data);
+	}
+	
+	public function actualizar_cliente($id_cliente = ""){
+		/*
+		echo "<pre>";
+			print_r($_POST);
+		echo "</pre>";	
+		exit;	
+		*/
+		if($_POST){
+						
 			$cliente_info =	$this->valida_datos_update();
 			$cliente_info['id_clienteIn'] = $id_cliente;
 				
-			if(!empty($this->login_errores)){					
-					echo "<pre>";
-						print_r($this->login_errores);
-					echo "</pre>";	
-			}
-			else{				
+			if(!empty($this->login_errores)){
+					$data['error'] = 1;					
+					$data['errores'] = $this->login_errores;					
+			} else{				
 				if($this->login_registro_model->actualizar_cliente($cliente_info)){
-					echo "actualizacion correcta";
-				}	
-				else{
-					echo "problema al actualizar";
+					$data['error'] = 0;							
+				} else{
+					$data['error'] = 1;
 				}				
 			}
-		}
 			
+		}
+				
+		echo json_encode($data);
+				
 	}
 	
 	public function valida_datos_update(){
--- application/views/reportes/reporte_compras_usuario.php
+++ application/views/reportes/reporte_compras_usuario.php
@@ -4,7 +4,12 @@
 </div>
 <?php
 	if($compras){
-	
+		/*
+		echo "<pre>";
+			print_r($compras);
+		echo "</pre>";
+		*/		
+		//echo md5('spin_vero@hotmail.com|A86aleman');
 		echo "	<table width='100%' cellpadding='0' cellspacing='0'>
 					<thead>					
 						<th>Fecha
@@ -17,62 +22,66 @@
 						</th>
 						<th>&nbsp;
 						</th>					
-					</thead>";	      		
+					</thead>";	      	
+		$icolor = 0;									
 		foreach($compras as $indice => $detalle_compra){
-			## intercalar color las filas
-			$color = array("#F1F1F1", "#E6E6E6");
-			if ($indice %2 == 0) {
-				$bck = "style='background-color: ".$color[0]."'";
-			}
-			else {
-				$bck = "style='background-color: ".$color[1]."'";
-			}						
-			echo "	<tr>
-						<td $bck >".$detalle_compra['compra']['fecha_compraDt']."
-						</td>
-						<td $bck>".$detalle_compra['compra']['id_compraIn']."
-						</td>
-						<td $bck>";							
-						foreach($detalle_compra['articulos'] as $articulo){
-							
-							//revisar si la descripcion de la promocion tiene slash's quitarlos
-							if(stristr($detalle_compra['promocion']->descripcionVc, "|")){
-								$mp=explode('|',$detalle_compra['promocion']->descripcionVc);
-								$nmp=count($mp);
-								if($nmp==2){
-									$desc_promo = $mp[0];		
-								}	
-								else if($nmp==3){
-									$desc_promo = $mp[1];
-								}								
-							}
-							else{
-								$desc_promo = $detalle_compra['promocion']->descripcionVc;	
-							}
-							//revisar si la descripcion del articulo tiene slash's quitarlos	
-							if(stristr($articulo['tipo_productoVc'], "|")){
-								$ma=explode('|',$articulo['tipo_productoVc']);
-								$nma=count($ma);
-								if($nma==2){
-									$desc_art = $ma[0];		
-								}	
-								else if($nma==3){
-									$desc_art = $ma[1];
-								}								
-							}
-							else{
-								$desc_art = $articulo['tipo_productoVc'];	
-							}
-							// mostrar la descripcion de promocion y articulo sin slash's
-							echo $desc_promo."<br />".
-								 $desc_art."<br />";							
-						}						
-			echo "		</td>
-						<td $bck align='right'>".number_format($detalle_compra['monto'], 2, '.', ',')."
-						</td>
-						<td $bck align='right'><a href=\"javascript: detalle_compra(".$detalle_compra['compra']['id_compraIn'].", ".$id_cliente.")\" >Ver detalle</a>
-						</td>  
-					</tr>";			
+			if($detalle_compra['respuesta_banco'] == "approved"){				
+				## intercalar color las filas
+				$color = array("#F1F1F1", "#E6E6E6");
+				if ($icolor %2 == 0) {
+					$bck = "style='background-color: ".$color[0]."'";
+				}
+				else {
+					$bck = "style='background-color: ".$color[1]."'";
+				}						
+				$icolor++;
+				echo "	<tr>
+							<td $bck >".$detalle_compra['compra']['fecha_compraDt']."
+							</td>
+							<td $bck>".$detalle_compra['compra']['id_compraIn']."
+							</td>
+							<td $bck>";							
+							foreach($detalle_compra['articulos'] as $articulo){
+								
+								//revisar si la descripcion de la promocion tiene slash's quitarlos
+								if(stristr($detalle_compra['promocion']->descripcionVc, "|")){
+									$mp=explode('|',$detalle_compra['promocion']->descripcionVc);
+									$nmp=count($mp);
+									if($nmp==2){
+										$desc_promo = $mp[0];		
+									}	
+									else if($nmp==3){
+										$desc_promo = $mp[1];
+									}								
+								}
+								else{
+									$desc_promo = $detalle_compra['promocion']->descripcionVc;	
+								}
+								//revisar si la descripcion del articulo tiene slash's quitarlos	
+								if(stristr($articulo['tipo_productoVc'], "|")){
+									$ma=explode('|',$articulo['tipo_productoVc']);
+									$nma=count($ma);
+									if($nma==2){
+										$desc_art = $ma[0];		
+									}	
+									else if($nma==3){
+										$desc_art = $ma[1];
+									}								
+								}
+								else{
+									$desc_art = $articulo['tipo_productoVc'];	
+								}
+								// mostrar la descripcion de promocion y articulo sin slash's
+								echo $desc_promo."<br />".
+									 $desc_art."<br />";							
+							}						
+				echo "		</td>
+							<td $bck align='right'>".number_format($detalle_compra['monto'], 2, '.', ',')."
+							</td>
+							<td $bck align='right'><a href=\"javascript: detalle_compra(".$detalle_compra['compra']['id_compraIn'].", ".$id_cliente.")\" >Ver detalle</a>
+							</td>  
+						</tr>";	
+			} 			
 		}
 		echo "</table>";	
 	}
