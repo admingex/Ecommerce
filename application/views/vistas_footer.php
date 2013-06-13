95789b61c69cb17484c321dbf5477a8383badb29
helladyo <harteaga@expansion.com.mx>
2013-06-13 12:42:03 -0500

    Author: harteaga Date: 13.06.2013 Description: Commit cambio de privacidad.

--- application/views/vistas_footer.php
+++ application/views/vistas_footer.php
@@ -10,33 +10,32 @@
 						$var=0;
 						switch($cargar_pagina) {
 							case("privacidad"):
-								include ('templates/privacidad.html');	
-								$var = 1;							
+								include ('templates/privacidad.html');
+								$var = 1;
 								break;
 							case("condiciones"):
 								include ('templates/condiciones.html');
 								break;
 							case("ayuda"):
 								include ('templates/ayuda.html');
-								$var = 1;																
+								$var = 1;
 								break;
 							case("contacto"):
 								include ('templates/contacto.html');
 								break;
 							default:
 								redirect('login', 'refresh', 303);
-								break;		
+								break;
 						}
 					} else {
 						redirect('login', 'refresh', 303);
 					}
 				?>
-					
 		</div> 
 	</div>
 </section>
 <?php 
-	if($var==1){
+	if ($var == 1) {
 		echo '<div style="width: 100px; float: right; margin-bottom: 20px"><a href='.site_url().' >Regresar</a></div>';
 	}
 ?>
